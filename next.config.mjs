e95bdfadf69c5741bdd1e3f66e3d03e05369e077
Dani el kebir <danielkebirnorea@gmail.com>
2024-11-05 18:46:06 +0100

    fix github deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Exporte le site en statique
+  basePath: '/Portfolio_norea_danielkebir', // Remplacez par le nom de votre dépôt GitHub
+  assetPrefix: '/Portfolio_norea_danielkebir/', // Précise le chemin pour les assets
+  images: {
+    unoptimized: true, // Nécessaire pour l'exportation statique si vous utilisez le composant Image de Next.js
+  },
+};
 
 export default nextConfig;
